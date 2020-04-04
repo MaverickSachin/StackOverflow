00892f643a6bd6e32ebf947470ec03be0095e6c1
Sachin Ailwar <SachinAilwar23@gmail.com>
2020-04-04 07:44:22 -0700

    StackOverflow: New Features for release v0.5
    StackOverflow: New Features for release v0.5
    Summary: Added - list of posts, reply to a post and added bunch of unit test cases
    Additional Notes: NA
    Dependencies: NA
    Fixed by: SachinAilwar23@gmail.com

--- boards/models.py
+++ boards/models.py
@@ -14,7 +14,7 @@ class Board(models.Model):
         return Post.objects.filter(topic__board=self).count()
 
     def get_last_post(self):
-        return Post.objects.filter(topic__board=self).order_by('-created_by').first()
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 
 class Topic(models.Model):
@@ -22,6 +22,7 @@ class Topic(models.Model):
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.subject
--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.models import User
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 
 
 def home(request):
@@ -12,7 +12,8 @@ def home(request):
 
 def topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'boards/topics.html', {'board': board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'boards/topics.html', {'board': board, 'topics': topics})
 
 
 @login_required
@@ -33,7 +34,7 @@ def new_topic(request, pk):
                 created_by=request.user
             )
 
-            return redirect('boards:topics', pk=board.pk)
+            return redirect('boards:posts', pk=board.pk, topic_pk=topic.pk)
 
     else:
         form = NewTopicForm()
@@ -43,4 +44,25 @@ def new_topic(request, pk):
 
 def posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
     return render(request, 'boards/posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+
+            return redirect('boards:posts', pk=pk, topic_pk=topic_pk)
+
+    else:
+        form = PostForm()
+
+    return render(request, 'boards/reply_topic.html', {'topic': topic, 'form': form})
